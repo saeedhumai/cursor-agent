3dddbb9814553667fa3228a38bcb8b8699a8d732
Nifemi Alpine Durin <femiamoo@juiceafrica.com>
2025-03-24 21:14:56 +0100

    fix imports

--- cursor_agent/agent/__init__.py
+++ cursor_agent/agent/__init__.py
@@ -9,10 +9,10 @@ from agent.factory import create_agent
 from agent.base import BaseAgent
 from agent.claude_agent import ClaudeAgent
 from agent.openai_agent import OpenAIAgent
-
+from agent.permissions import PermissionOptions
 # Import interactive functionality
 from agent.interact import run_agent_interactive, run_agent_chat
 
 # Define what should be available when using "from cursor_agent.agent import *"
 __all__ = ["create_agent", "BaseAgent", "ClaudeAgent", "OpenAIAgent", 
-           "run_agent_interactive", "run_agent_chat"] 
+           "run_agent_interactive", "run_agent_chat", "PermissionOptions"] 
